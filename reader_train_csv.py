f729d565034a7643719bf70fa731ea2f1d6b3ec3
admin <zhoudu@bupt.edu.cn>
2018-03-17 13:59:18 +0800

    修改reader_train_csv.py

--- reader_train_csv.py
+++ reader_train_csv.py
@@ -4,16 +4,17 @@ from itertools import islice
 data_path = "/home/sk39/workspace/dataset/tianchi_clothes/train/Annotations/annotations.csv"
 test_path = "test.csv"
 f = open(test_path, "r")
-keys = f.readline()  # 读取第一行的内容
+keys = f.readline().split(',')  # 读取第一行的内容
 print(keys)
 f.close()
 imgs_dict = {}
 data_table = []
 with open(test_path, "r") as f:
     for value in islice(f, 1, None):
-        print(value)
-        imgs_dict[keys[i]] = v
+        v = value.split(',')
+        print(v)
+
 # with open(test_path, "r") as f:
 #     reader = csv.DictReader(f)
 #     rows = [row for row in reader]
-print(rows)
+# print(rows)
